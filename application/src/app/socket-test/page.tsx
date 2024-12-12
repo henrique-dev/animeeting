f00455109817fc48f9427234b0bb2724add16f0f
Paulo Bacelar <henrique.phgb@gmail.com>
2024-12-11 20:19:55 -0800

    Add i18n

---
+++ application/src/app/socket-test/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { socket } from '@/socket';
+import { useCallback, useEffect, useState } from 'react';
+
+const Page = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState('N/A');
+
+  const onConnect = useCallback(() => {
+    setIsConnected(true);
+    setTransport(socket.io.engine.transport.name);
+
+    socket.io.engine.on('upgrade', (transport) => {
+      setTransport(transport.name);
+    });
+  }, [setIsConnected, setTransport]);
+
+  const onDisconnect = useCallback(() => {
+    setIsConnected(false);
+    setTransport('N/A');
+  }, [setIsConnected, setTransport]);
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, [onConnect, onDisconnect]);
+
+  return (
+    <div>
+      <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
+      <p>Transport: {transport}</p>
+    </div>
+  );
+};
+
+export default Page;
