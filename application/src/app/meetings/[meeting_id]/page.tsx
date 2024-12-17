0e7d0a0776eb9acb24a52a347ff557a318e16218
Paulo Bacelar <henrique.phgb@gmail.com>
2024-12-16 20:50:54 -0800

    Updating some packages

--- application/src/app/meetings/[meeting_id]/page.tsx
+++ application/src/app/meetings/[meeting_id]/page.tsx
@@ -1,6 +1,6 @@
 import { MeetingPage } from '@/components/pages/meeting';
 
-const Page = async ({ params }: { params: { meeting_id: string } }) => {
+const Page = async ({ params }: { params: Promise<{ meeting_id: string }> }) => {
   const { meeting_id } = await params;
 
   return <MeetingPage meetingId={meeting_id} />;
